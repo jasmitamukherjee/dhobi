f86c3d9f42399f4505c8fa42e511ac3b19750000
jasmitamukherjee <jasmita.mukherjee.btech2021@sitpune.edu.in>
2024-03-18 23:49:38 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './components/Footer';
 import Navbar from './components/Navbar';
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
     
     <div>
       <Navbar/>
-      Dhobi
+      <Footer/>
     </div>
   );
 }
